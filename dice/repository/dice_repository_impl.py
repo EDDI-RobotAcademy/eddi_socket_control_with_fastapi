d1ef574542cc5e81ad116d4f707988abf19600b3
silenc3502 <gcccompil3r@gmail.com>
2024-08-08 21:27:41 +0900

    fix: 동기 방식으로 인해 무한 대기 걸리는 문제 비동기 방식으로 변경하여 해결

--- dice/repository/dice_repository_impl.py
+++ dice/repository/dice_repository_impl.py
@@ -1,4 +1,5 @@
 import json
+import queue
 
 from dice.repository.dice_repository import DiceRepository
 
@@ -6,6 +7,10 @@ from dice.repository.dice_repository import DiceRepository
 class DiceRepositoryImpl(DiceRepository):
     def listResult(self, systemReceiverFastAPIChannel):
         print(f"DiceRepositoryImpl listResult()")
-        receivedResponseFromSocketClient = systemReceiverFastAPIChannel.get()
 
-        return json.loads(receivedResponseFromSocketClient)
+        try:
+            receivedResponseFromSocketClient = systemReceiverFastAPIChannel.get(False)
+            return json.loads(receivedResponseFromSocketClient)
+
+        except queue.Empty:
+            return "아직 데이터를 처리 중이거나 요청한 데이터가 없습니다"
