3bc81cf528ccb04863721229e1cc8b5541cd8947
Sanghoon Lee <gcccompil3r@gmail.com>
2024-08-13 11:52:26 +0900

    feat: CORS 문제에 대응

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 import os.path
-import sys
+from fastapi.middleware.cors import CORSMiddleware
 
 import colorama
 
 import uvicorn
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
 from deep_learning.controller.deep_learning_controller import deepLearningRouter
@@ -16,6 +17,18 @@ DomainInitializer.initEachDomain()
 SystemInitializer.initSystemDomain()
 
 app = FastAPI()
+
+load_dotenv()
+
+origins = os.getenv("ALLOWED_ORIGINS", "").split(",")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(deepLearningRouter)
 app.include_router(diceResultRouter)
