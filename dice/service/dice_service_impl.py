92e4f284e6a0a53ea8365654535b79b259932e77
Sanghoon Lee <gcccompil3r@gmail.com>
2024-08-27 13:40:09 +0900

    add: 디버깅 서포트 메시지 추가

--- dice/service/dice_service_impl.py
+++ dice/service/dice_service_impl.py
@@ -1,5 +1,6 @@
 from dice.repository.dice_repository_impl import DiceRepositoryImpl
 from dice.service.dice_service import DiceService
+from include.socket_server.utility.color_print import ColorPrinter
 from system_queue.repository.system_queue_repository_impl import SystemQueueRepositoryImpl
 
 
@@ -10,6 +11,7 @@ class DiceServiceImpl(DiceService):
 
     def requestListResult(self):
         systemReceiverFastAPIChannel = self.__systemQueueRepository.getSystemSocketReceiverFastAPIChannel()
+        ColorPrinter.print_important_data("systemReceiverFastAPIChannel", systemReceiverFastAPIChannel)
         return self.__diceRepository.listResult(systemReceiverFastAPIChannel)
 
 
