41145aad407c69876d41c170a9a50cc6045fdc15
Sanghoon Lee <gcccompil3r@gmail.com>
2024-10-30 10:52:03 +0900

    fix: 사용하지 않는 필드 제거하여 혼선 방지

--- deep_learning/controller/request_form/ai_command_request_form.py
+++ deep_learning/controller/request_form/ai_command_request_form.py
@@ -5,9 +5,8 @@ from deep_learning.service.request.ai_command_request import AICommandRequest
 
 
 class AICommandRequestForm(BaseModel):
-    userToken: str
     command: int
     data: Optional[Union[Any, List[Any]]] = None
 
     def toAICommandRequest(self) -> AICommandRequest:
-        return AICommandRequest(userToken=self.userToken, command=self.command, data=self.data)
+        return AICommandRequest(command=self.command, data=self.data)
